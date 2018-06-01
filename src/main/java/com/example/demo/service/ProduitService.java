df51a77a4c119a3e4da4da38bb3083a1411a4c0a
Zitouni mohamed <zitouni.mohamed.email@gmail.com>
2018-06-01 17:23:38 +0200

    sprin security

--- src/main/java/com/example/demo/service/ProduitService.java
+++ src/main/java/com/example/demo/service/ProduitService.java
@@ -1,41 +1,40 @@
-package com.example.demo.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.Produit;
-import com.example.demo.repository.ProduitRepository;
-
-@Service
-@Primary
-public class ProduitService implements IProduitService{
-
-	@Autowired
-	private ProduitRepository produitRepository;
-	
-	@Override
-	public List<Produit> getProduits() {
-		return produitRepository.findAll();
-	}
-
-	@Override
-	public void addProduit(Produit produit) {
-		produitRepository.save(produit);
-	}
-
-	@Override
-	public void updateProduit(Produit produit) {
-		produitRepository.save(produit);
-	}
-
-	@Override
-	public void deleteProduit(Long id) {
-		Produit produit = new Produit();
-		produit.setId(id);
-		produitRepository.delete(produit);
-	}
-
-}
+//package com.example.demo.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Service;
+//
+//import com.example.demo.entity.Produit;
+//import com.example.demo.repository.ProduitRepository;
+//
+//@Service
+//public class ProduitService implements IProduitService{
+//
+//	@Autowired
+//	private ProduitRepository produitRepository;
+//	
+//	@Override
+//	public List<Produit> getProduits() {
+//		return produitRepository.findAll();
+//	}
+//
+//	@Override
+//	public void addProduit(Produit produit) {
+//		produitRepository.save(produit);
+//	}
+//
+//	@Override
+//	public void updateProduit(Produit produit) {
+//		produitRepository.save(produit);
+//	}
+//
+//	@Override
+//	public void deleteProduit(Long id) {
+//		Produit produit = new Produit();
+//		produit.setId(id);
+//		produitRepository.delete(produit);
+//	}
+//
+//}
