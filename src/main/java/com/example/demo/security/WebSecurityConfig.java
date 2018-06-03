dc8dd0e16996baba270b1f2d8a50719bbf97e0fe
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2018-06-03 19:13:38 +0200

    optimisation

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -5,24 +5,27 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		
+	// Authorization : Role -> Access
+			protected void configure(HttpSecurity http) throws Exception {
+				http.httpBasic().and().authorizeRequests().antMatchers("/api/**")
+						.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
+						.csrf().disable().headers().frameOptions().disable();
+			}
+			
 	// Authentication : User --> Roles
 		protected void configure(AuthenticationManagerBuilder auth)
 				throws Exception {
 			auth.inMemoryAuthentication().passwordEncoder(
-					org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
+					NoOpPasswordEncoder.getInstance())
 			.withUser("user").password("password1")
 					.roles("USER").and().withUser("admin").password("password2")
 					.roles("USER", "ADMIN");
-		}
-
-		// Authorization : Role -> Access
-		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic().and().authorizeRequests().antMatchers("/api/**")
-					.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
-					.csrf().disable().headers().frameOptions().disable();
 		}
 }
