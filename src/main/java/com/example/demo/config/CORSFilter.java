5a8b98849b5d272dcf6afd491260479882cfce6d
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2018-06-06 20:03:12 +0200

    add PUT method to Cros Filter chain

--- src/main/java/com/example/demo/config/CORSFilter.java
+++ src/main/java/com/example/demo/config/CORSFilter.java
@@ -30,7 +30,7 @@ public class CORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) resp;
 		HttpServletRequest request = (HttpServletRequest) req;
 		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Headers",
